024cece9b76426361569c3f2de46d1f4b8faeef0
Guilherme Nogueira <guilherme.nogueira11@fatec.sp.gov.br>
2021-03-12 22:41:02 -0300

    api OK

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Button, 
+  FlatList, 
+  Keyboard, 
+  StyleSheet, 
+  Text,
+  TextInput, 
+  View 
+} from 'react-native';
+
+
+import keys from './keys';
+import PrevisaoItem from './components/PrevisaoItem';
 
 export default function App() {
+  const [cidade, setCidade] = useState("");
+  const [latitute, setLatitude] = useState("");
+  const [longitude, setLongitude] = useState("");
+  const [dados, setDados] = useState("");
+  const capturarCidade = (cidade) => {
+    setCidade(cidade);
+  }
+  const obterLatandLong = () => {
+    setDados("");
+    setLatitude("");
+    setLongitude("");
+    const target =  `${endPoint}${cidade}&appid=${apiKey}`;
+    fetch(target)
+    .then((data) => data.json())
+    .then((data) => {
+      setLatitude(data.city.coord.lat)
+      setLongitude(data.city.coord.lon)
+    }).then(
+      obterSun
+    );
+    
+  }
+  const obterSun = () => {
+    const target = `${endPointSun}lat=${latitute}&lon=${longitude}&appid=${apiKey}`
+    fetch(target)
+    .then((data) => data.json())
+    .then((data) => {
+      setDados(data.current)
+    });
+  }
+  const endPoint = `https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=`;
+  const endPointSun = `https://api.openweathermap.org/data/2.5/onecall?units=metric&lang=pt_br&`
+  const apiKey = keys.weatherMapApiKey;
   return (
     <View style={styles.container}>
-      <Text>Blank Project</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput 
+          style={styles.nomeCidade}
+          placeholder="Digite o nome da cidade"
+          value={cidade}
+          onChangeText={capturarCidade}
+        />
+        <Button 
+          title="OK"
+          onPress={obterLatandLong}
+        /> 
+        <Button 
+          title="OK2"
+          onPress={obterSun}
+        /> 
+      </View>
+     
+      <FlatList 
+        data={dados}
+        renderItem={          
+          data =>(
+            <PrevisaoItem data={data} />
+          )
+        }
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    padding: 60,
+    flexDirection: "column",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white"
   },
+  nomeCidade: {
+    padding: 12,
+    borderBottomColor: "#BB96F3",
+    borderBottomWidth: 2,
+    textAlign: "center",
+    marginBottom: 8
+  },
+  entrada: {
+    marginBottom: 12
+  }
 });
---
+++ components/PrevisaoItem.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { 
+  Image,
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
+
+import Cartao from './Cartao'
+
+const PrevisaoItem = (props) => {
+  return (
+    <Cartao estilos={styles.cartao}>
+      <View style={styles.tela}>
+        <Image 
+          style={styles.imagem}
+          source={{ uri: `https://openweathermap.org/img/wn/${props.data.weather[0].icon}.png`}}
+        />
+        <View>
+          <View style={styles.segundaLinha}>
+            <Text style={styles.valor}>
+              {`Sunrise: ${props.data.sunrise}`}
+            </Text>
+            <Text style={styles.valor}>
+              {`Sunset: ${props.data.sunset}`}
+            </Text>
+            <Text style={styles.valor}>
+              {`Feels Like: ${props.data.feels_like}`}
+            </Text>
+          </View>
+        </View>
+      </View>
+    </Cartao>
+  )
+};
+
+export default PrevisaoItem;
+
+const styles = StyleSheet.create({
+  cartao: {
+    marginBottom: 8
+  },
+  tela: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'space-evenly'
+  },
+  primeiraLinha: {
+    marginTop: 7,
+    justifyContent: 'center',
+    flexDirection: 'row'
+  },
+  segundaLinha: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 4,
+    borderTopWidth: 1,
+    borderTopColor: '#DDD'
+  },
+  imagem: {
+    width: 50,
+    height: 50
+  },
+  valor: {
+    marginHorizontal: 2
+  }
+});
