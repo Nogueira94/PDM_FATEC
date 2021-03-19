976f9ca57cd408942eb3f4ce33175a0eaeed15cf
Victor Duarte Venancio <victor.duarte@ibm.com>
2021-03-19 14:03:47 -0300

    Versao final

---
+++ components/Cartao.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { View, StyleSheet} from 'react-native';
+
+const Cartao = (props) => {
+    return (
+        <View style={{...estilos.cartao, ...props.estilos}}>
+            {props.children}
+        </View>
+    );
+}
+
+const estilos = StyleSheet.create({
+    cartao: {
+     justifyContent: 'center',
+     alignItems: 'center',
+     shadowColor: 'black',
+     textShadowOffset: {
+         width: 0,
+         height: 2
+     },
+     shadowRadius: 6,
+     shadowOpacity: 0.32,
+     padding: 12,
+     borderRadius: 12
+     
+    }
+})
+
+export default Cartao;
